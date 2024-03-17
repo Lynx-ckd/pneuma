112f6afe08a8b4216d629b7d4e66508960ae9150
Rosant Ckd <36919741+Lynx-ckd@users.noreply.github.com>
2024-03-17 22:48:53 +0545

    Add files via upload

---
+++ pneuma/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pneuma Sign</title> 
+
+    <meta name="description" content="">
+
+    <meta name="robots" content="index,follow,noodp"><!-- All Search Engines -->
+    <meta name="googlebot" content="index,follow"><!-- Google Specific -->
+
+    <!-- Disable automatic detection and formatting of possible phone numbers -->
+    <meta name="format-detection" content="telephone=no">
+
+    <!-- Google font -->
+    <!-- <link href="assets/fonts/pristina.TTF" rel="stylesheet" type="text/css"/> -->
+    <link href="https://fonts.googleapis.com/css?family=Italianno" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    
+    
+</head>
+<body>
+    <h1 style="text-align: center; margin-top:25%;">Under construction !!!!</h1>
+<main>
+	
+</main>
+
+</body>
+    </html>
